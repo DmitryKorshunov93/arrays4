33c95af438dc4445ecfa0a8629fd9ba0c0e3ed07
De93m <DmitryKorshunov93@gmail.com>
2023-09-17 14:44:52 +0300

    arrays4 доработки

--- src/Main.java
+++ src/Main.java
@@ -68,7 +68,7 @@ public class Main {
         int[] salary = generateRandomArray();
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
+            System.out.print(reverseFullName[i]) ;
 
         }
     }
